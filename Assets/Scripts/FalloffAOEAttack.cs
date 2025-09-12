8ab4dce423efe3d8e0a0fab6e09b606493a702ff
MoolmanM <mmoolman067@gmail.com>
2025-09-13 00:54:58 +0200

    fix: find attack range

--- Assets/Scripts/FalloffAOEAttack.cs
+++ Assets/Scripts/FalloffAOEAttack.cs
@@ -7,17 +7,13 @@ public class FalloffAOEAttack : Attack
   public float minDamage;
   public float range;
 
-  public override void Execute(RectTransform attacker, Enemy target, Enemy[] allEnemies)
+  public override void Execute(RectTransform attacker, Enemy target, Enemy[] inRange)
   {
-    foreach (var enemy in allEnemies)
+    foreach (var enemy in inRange)
     {
-      float dist = Vector2.Distance(target.RectTransform.anchoredPosition, enemy.RectTransform.anchoredPosition);
-      if (dist <= range)
-      {
-        float t = dist / range;
-        float dmg = Mathf.Lerp(maxDamage, minDamage, t);
-        enemy.TakeDamage(dmg);
-      }
+      //float t = dist / range;
+      //float dmg = Mathf.Lerp(maxDamage, minDamage, t);
+      //enemy.TakeDamage(dmg);
     }
   }
 }
