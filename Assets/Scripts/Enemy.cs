c7642965317a3b14b119f5d35cd871f1eae407bc
MoolmanM <mmoolman067@gmail.com>
2025-09-12 22:02:51 +0200

    feat: add Enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class Enemy : MonoBehaviour
+{
+  [SerializeField] private float maxHealth = 100f;
+  [SerializeField] private Slider healthBar;
+  [SerializeField] private TMP_Text healthText;
+
+  private float currentHealth;
+
+  public event Action<Enemy> OnDeath;
+
+  private void Awake()
+  {
+    currentHealth = maxHealth;
+    UpdateUI();
+  }
+
+  public void TakeDamage(float amount)
+  {
+    currentHealth -= amount;
+    if (currentHealth < 0) currentHealth = 0;
+
+    UpdateUI();
+
+    if (currentHealth <= 0)
+      Die();
+  }
+
+  private void UpdateUI()
+  {
+    if (healthBar != null)
+      healthBar.value = currentHealth / maxHealth;
+
+    if (healthText != null)
+      healthText.text = $"{currentHealth}/{maxHealth}";
+  }
+
+  private void Die()
+  {
+    OnDeath?.Invoke(this);
+    gameObject.SetActive(false);
+  }
+
+  public bool IsDead => currentHealth <= 0;
+  public RectTransform RectTransform => (RectTransform)transform;
+}
