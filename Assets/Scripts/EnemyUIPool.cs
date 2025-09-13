46fbd02da004652e69dc9a824bd823597be169b0
MoolmanM <mmoolman067@gmail.com>
2025-09-13 22:19:41 +0200

    feat: add EnemyUIPool

---
+++ Assets/Scripts/EnemyUIPool.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class EnemyUIPool : MonoBehaviour
+{
+  [SerializeField] private GameObject enemyUIPrefab;
+  [SerializeField] private Transform enemyParent;
+
+  private Queue<EnemyUIController> pool = new Queue<EnemyUIController>();
+
+  public EnemyUIController Get(Enemy enemyData)
+  {
+    EnemyUIController ui;
+    if (pool.Count > 0)
+    {
+      ui = pool.Dequeue();
+      ui.gameObject.SetActive(true);
+    }
+    else
+    {
+      GameObject obj = Instantiate(enemyUIPrefab, enemyParent);
+      ui = obj.GetComponent<EnemyUIController>();
+    }
+
+    ui.Bind(enemyData);
+    return ui;
+  }
+
+  public void Return(EnemyUIController ui)
+  {
+    ui.gameObject.SetActive(false);
+    pool.Enqueue(ui);
+  }
+}
