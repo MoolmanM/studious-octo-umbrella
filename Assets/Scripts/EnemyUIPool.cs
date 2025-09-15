35b105aecaf7c62a0e7c8324fdbe1af9452e61f4
MoolmanM <mmoolman067@gmail.com>
2025-09-15 22:40:08 +0200

    style: adjust indentation for improved readability in EnemyUIPool.cs

--- Assets/Scripts/EnemyUIPool.cs
+++ Assets/Scripts/EnemyUIPool.cs
@@ -3,32 +3,32 @@ using System.Collections.Generic;
 
 public class EnemyUIPool : MonoBehaviour
 {
-  [SerializeField] private GameObject enemyUIPrefab;
-  [SerializeField] private Transform enemyParent;
+    [SerializeField] private GameObject enemyUIPrefab;
+    [SerializeField] private Transform enemyParent;
 
-  private Queue<EnemyUIController> pool = new Queue<EnemyUIController>();
+    private Queue<EnemyUIController> pool = new Queue<EnemyUIController>();
 
-  public EnemyUIController Get(Enemy enemyData)
-  {
-    EnemyUIController ui;
-    if (pool.Count > 0)
+    public EnemyUIController Get(Enemy enemyData)
     {
-      ui = pool.Dequeue();
-      ui.gameObject.SetActive(true);
+        EnemyUIController ui;
+        if (pool.Count > 0)
+        {
+            ui = pool.Dequeue();
+            ui.gameObject.SetActive(true);
+        }
+        else
+        {
+            GameObject obj = Instantiate(enemyUIPrefab, enemyParent);
+            ui = obj.GetComponent<EnemyUIController>();
+        }
+
+        ui.Bind(enemyData);
+        return ui;
     }
-    else
+
+    public void Return(EnemyUIController ui)
     {
-      GameObject obj = Instantiate(enemyUIPrefab, enemyParent);
-      ui = obj.GetComponent<EnemyUIController>();
+        ui.gameObject.SetActive(false);
+        pool.Enqueue(ui);
     }
-
-    ui.Bind(enemyData);
-    return ui;
-  }
-
-  public void Return(EnemyUIController ui)
-  {
-    ui.gameObject.SetActive(false);
-    pool.Enqueue(ui);
-  }
 }
