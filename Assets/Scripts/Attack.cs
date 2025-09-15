4aec0c00c8b95e84b173ac4a07eccb01fc1def58
MoolmanM <mmoolman067@gmail.com>
2025-09-15 22:39:42 +0200

    style: format code for consistency in Attack.cs

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public abstract class Attack : ScriptableObject
 {
-  [SerializeField] private bool useRange = false;
-  [SerializeField] public float rangeX;
-  [SerializeField] public float rangeY;
+    [SerializeField] private bool useRange = false;
+    [SerializeField] public float rangeX;
+    [SerializeField] public float rangeY;
 
-  public abstract void Execute(RectTransform attacker, Enemy target, Enemy[] inRange);
+    public abstract void Execute(RectTransform attacker, Enemy target, Enemy[] inRange);
 
-  public bool HasRange => useRange;
+    public bool HasRange => useRange;
 }
