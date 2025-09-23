4cab40220313b30c16163e5ecc0c8b88332217f4
MoolmanM <mmoolman067@gmail.com>
2025-09-23 23:16:07 +0200

    refactor: replace hardcoded range with IAreaSelector strategy

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public abstract class Attack : ScriptableObject
 {
-    [SerializeField] private bool useRange = false;
-    [SerializeField] public float rangeX;
-    [SerializeField] public float rangeY;
+    [SerializeField] private IAreaSelector areaSelector;
+    [SerializeField] public float attackSpeed = 1f;
+    [SerializeField] public float manaCost = 1f;
 
     public abstract void Execute(RectTransform attacker, Enemy target, Enemy[] inRange);
 
-    public bool HasRange => useRange;
+    public Enemy[] GetEnemiesInRange(Enemy target, Enemy[] allEnemies)
+    {
+        return areaSelector?.GetEnemiesInArea(target, allEnemies) ?? new Enemy[0];
+    }
 }
