edcf29032c1402ad9d441741f15c61b41001b6d2
MoolmanM <mmoolman067@gmail.com>
2025-09-27 23:41:18 +0200

    feat: add MysticCaveLocation ScriptableObject

---
+++ Assets/Scripts/MysticCavesLocation.cs
@@ -0,0 +1,8 @@
+
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Location/Mystic Cave")]
+public class MysticCaveLocation : ScriptableObject
+{
+    public string locationName = "Mystic Cave";
+}
